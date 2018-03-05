8d89c0f5c84aef5308950da4f30f67d085d5d94c
Keith Light <keith.light@strawberrysoup.co.uk>
2018-03-05 20:36:54 +0000

    Amend response to JSON

--- isitup.php
+++ isitup.php
@@ -63,20 +63,22 @@ $response_array = json_decode($ch_response, true);
 # You can use any emoji that is available to your Slack team, including the custom ones.
 if ($ch_response === FALSE) {
     # isitup.org could not be reached
-    $reply = "Ironically, isitup could not be reached.";
+    $reply['response_type'] = 'ephemeral';
+    $reply['text'] = 'Ironically, isitup could not be reached.';
 } else {
-    if ($response_array["status_code"] == 1) {
+    $reply['response_type'] = 'in_channel';
+    if ($response_array['status_code'] == 1) {
         # Yay, the domain is up!
-        $reply = ":thumbsup: I am happy to report that *<http://" . $response_array["domain"] . "|" . $response_array["domain"] . ">* is *up*!";
+        $reply['text'] = ":thumbsup: I am happy to report that *<http://" . $response_array["domain"] . "|" . $response_array["domain"] . ">* is *up*!";
     } else if ($response_array["status_code"] == 2) {
         # Boo, the domain is down.
-        $reply = ":disappointed: I am sorry to report that *<http://" . $response_array["domain"] . "|" . $response_array["domain"] . ">* is *not up*!";
+        $reply['text'] = ":disappointed: I am sorry to report that *<http://" . $response_array["domain"] . "|" . $response_array["domain"] . ">* is *not up*!";
     } else if ($response_array["status_code"] == 3) {
         # Uh oh, isitup.org doesn't think the domain entered by the user is valid
-        $reply = ":interrobang: *" . $text . "* does not appear to be a valid domain. \n";
-        $reply .= "Please enter both the domain name AND suffix (example: *amazon.com* or *whitehouse.gov*).";
+        $reply['text'] = ":interrobang: *" . $text . "* does not appear to be a valid domain. \n";
+        $reply['text'] .= "Please enter both the domain name AND suffix (example: *amazon.com* or *whitehouse.gov*).";
     }
 }
 
 # Send the reply back to the user. 
-echo $reply;
+echo json_encode($reply);
